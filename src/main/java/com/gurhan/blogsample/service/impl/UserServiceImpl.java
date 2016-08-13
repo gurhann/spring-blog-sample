46a87b369e62629f43c4433ef681893868b9bf63
Gürhan <gurhan.kucuk@yandex.com>
2016-08-13 13:28:26 +0300

    fix

--- src/main/java/com/gurhan/blogsample/service/impl/UserServiceImpl.java
+++ src/main/java/com/gurhan/blogsample/service/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.gurhan.blogsample.service.impl;
 
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gurhan.blogsample.constant.Role;
 import com.gurhan.blogsample.mapper.UserMapper;
